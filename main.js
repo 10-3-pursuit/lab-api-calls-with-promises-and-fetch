1becd4d897ea7cb46e22490e2914b79d664550ab
Cristian Garcia <crisg@sas.upenn.edu>
2023-11-10 14:10:53 -0500

    created elements article, h2, p1, button and p2

--- main.js
+++ main.js
@@ -3,3 +3,11 @@ const url = "https://opentdb.com/api.php?amount=10";
 function displayError(error) {
     console.log(error);
 }
+
+function createTriviaQuestion(category, question, answer) {
+    const article = document.createElement("article");
+    const h2 = document.createElement("h2");
+    const p1 = document.createElement("p");
+    const button = document.createElement("button");
+    const p2 = document.createElement("p");
+}
