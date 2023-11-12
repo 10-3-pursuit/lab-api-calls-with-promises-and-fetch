4d27a37b314f6d571bd3a13e4990fc7ffa20e697
MarlonPelau <MarlonRegis@pursuit.org>
2023-11-12 13:32:44 -0500

    trying to figure out the .js file logic

--- main.js
+++ main.js
@@ -1,16 +1,31 @@
-// function displayCard(person) {
-//     const { title, first, last } = person.name;
-//     const fullName = `${title} ${first} ${last}`;
-//     const peopleClass = document.querySelector(".people");
+function displayError(error) {
+    const section = document.querySelector("section.error");
+    section.style.display = "block";
   
-//     peopleClass.innerHTML += `<section class="card">
-//     <img src=${person.picture.large} alt="${fullName}"/>
-//     <h2>${fullName}</h2>
-//     <p>${person.email}</p>
-//     </section>`;
-//   }
+    section.innerHTML = `
+    <p>Something went wrong!</p>
+    <p class="error-message">${error}</p>
+    `;
+  }
 
-//   fetch("https://randomuser.me/api/?results=5")
-//   .then((response) => response.json())
-//   .then((data) => data.results.forEach((user) => displayCard(user)))
-//   .catch((error) => displayError(error));
+function displayCard(trivia) {
+    const { question, correct_answer } = trivia;
+    const eachQuestion = `${question} ${correct_answer}`;
+    const cardClass = document.querySelector(".card");
+  
+    cardClass.innerHTML += `<section class="card">
+    <h2>${eachQuestion}</h2>
+    <p>${trivia}</p>
+    </section>`;
+  }
+const form = document.querySelector("form");
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+    const numUser = event.target.quantity.value;
+    console.log(numUser)
+} )
+
+  fetch("https://opentdb.com/api.php?amount=10&category=12&difficulty=easy&type=boolean&encode=url3986")
+  .then((response) => response.json())
+  .then((data) => data.results.forEach((user) => displayCard(user)))
+  .catch((error) => displayError(error));
