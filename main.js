c900c97c7ce6c9601ad42002cb2e3991f6955e35
enochtagoe <enochtagoe@pursuit.org>
2023-11-12 21:45:12 -0500

    submit updates

--- main.js
+++ main.js
@@ -46,41 +46,7 @@ function getQuestions() {
     })
     
 }
-getQuestions()
-
-// const button = document.querySelector('button')
-
-// button.addEventListener('submit', (event) => {
-//     event.preventDefault()
-//     // I need to gr ab the value given in the form
-//     const numUser = event.target.quantity.value
-//     // console.log(numUser)
-
-//     // add the fetch
-//     fetch("https://opentdb.com/api.php?amount=10")
-//     // fetch(`https://randomuser.me/api/?results=${numUser}`)
-//       .then((response) => response.json())
-//       .then((data) => data.results.forEach((user) => displayCard(user)))
-//       .catch((error) => displayError(error));
-// })
-
-
-// //i am making a call to the randomuser api using the fetch function
-// fetch("https://randomuser.me/api/?results=5")
-//   .then((response) => {
-//     // i use the .then for my success and am converting my results into JSON I must return the json in order to pass it to the next .then
-//     return response.json();
-//   })
-//   .then((data) => {
-//     // i use the .then to view the actual data I am receiving. I am the one to name the variable name in the argument. 
-
-//     // in fetch every data object has a results key that holds an array. so i must key into it.
-
-//     // I must loop through the array to manipulate and render each user object
-//     data.results.forEach((user) => displayCard(user));
-//   })
-//   .catch((error) => displayError(error));
-
+getQuestions();
 
 
 
