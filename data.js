c5835be551b83a21d4605c558b302f3a18768fb6
Haiyah Perez <haiyahperez@Haiyahs-Air.lan>
2023-11-12 21:19:15 -0500

    done

--- data.js
+++ data.js
@@ -23,10 +23,3 @@ newTrivia.addEventListener("click", (event) => {
                 });
             });
     });
-
-//   <article class="card">
-//      <h2>CATEGORY</h2>
-//      <p>QUESTION</p>
-//      <button>Show Answer</button>
-//      <p class="hidden">CORRECT ANSWER</p>
-//   </article>
